9b3c03a0a417090f9c493571fbd626ff605da9a2
Gitizen <git-i-zen-hub@github.com>
2023-05-19 17:16:53 +0530

    list imported

--- Day-10/title.py
+++ Day-10/title.py
@@ -16,6 +16,9 @@
 # Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 # Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
+from pyparsing import List
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l, r = 0, 1
